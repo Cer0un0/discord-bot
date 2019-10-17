9af59c81e30aad7caf0c11cdc2732ff9050e737b
satory074 <satory074@gmail.com>
2019-10-17 23:26:19 +0900

    fix

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import discord
 #import chatbot
 
 # 自分のBotのアクセストークンに置き換えてください.
-TOKEN = 'NjMyMTAzODA2OTg5MTA3MjAx.Xahv4A.iDl1JTtxxJGWBKratbjh9fiBamk'
+TOKEN = 'NjMyMTAzODA2OTg5MTA3MjAx.Xah59w.A6HEp3MSk98xQFS-_SP6cMKCGPI'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
