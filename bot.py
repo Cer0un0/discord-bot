9faa31ec1673dbd65aeb0fed0ae4c2ace4970f34
satory074 <satory074@gmail.com>
2019-10-22 00:45:34 +0900

    test

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ async def on_message(message):
     # オウム返し
     for qu in message.content.split():
         await message.channel.send(message.content.split())
-        await message.channel.send(dict_repetition.keys())
+        await message.channel.send(qu in dict_repetition.keys())
         continue
 
         if qu == '/neko':
