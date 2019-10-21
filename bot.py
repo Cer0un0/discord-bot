95228625db85a99c38a0afdfa69aac1f59a74e3a
satory074 <satory074@gmail.com>
2019-10-22 00:20:38 +0900

    test

--- bot.py
+++ bot.py
@@ -102,9 +102,7 @@ async def on_message(message):
         if qu == '/neko':
             await message.channel.send(msg_neko())
 
-        await message.channel.send(qu)
-        await message.channel.send(repeat_query.keys())
-        if qu in repeat_query.keys():
+        if qu in dict_repetition.keys():
             await message.channel.send(msg_repetition(qu))
 
         if qu == '/omikuji':
