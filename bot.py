a156ebfb0418e1aaf83ed8b509693ec5c32defdb
satory074 <satory074@gmail.com>
2019-10-22 21:25:57 +0900

    debug

--- bot.py
+++ bot.py
@@ -142,8 +142,7 @@ def msg_dice(pattern, qu):
         ダイス結果メッセージ
     """
 
-    return re.match(pattern, qu).group()
-    n, me = map(int, re.match(pattern, qu).group())
+    n, me = map(int, re.match(pattern, qu).group().split("d"))
     reply = ""
     for i in range(n):
         reply += f"{ra.randrange(me) + 1}"
