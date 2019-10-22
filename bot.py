12a5138e79d2cd7b41aa0e6a23069dfeb8b707e0
satory074 <satory074@gmail.com>
2019-10-22 21:40:40 +0900

    debug

--- bot.py
+++ bot.py
@@ -146,8 +146,7 @@ def msg_dice(pattern, qu):
     dice = [ra.randrange(me) + 1 for i in range(n)]
     reply =  '' if re.split(pattern, qu)[0] == '' else f"{re.split(pattern, qu)[0]}: "
 
-    # return f"{reply}: {', '.join(dice)} ({sum(dice)})"
-    return ' '.join(dice)
+    return f"{reply}: {', '.join(map(str, dice))} ({sum(dice)})"
 #
 #
 async def do_slot(qu, message):
