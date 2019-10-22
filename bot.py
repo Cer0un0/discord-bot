e98ae860110e8db66d46ff8088fb24a26f35c6c7
satory074 <satory074@gmail.com>
2019-10-23 02:03:26 +0900

    debug

--- bot.py
+++ bot.py
@@ -218,7 +218,7 @@ async def on_message(message):
 
         # おちんぽプログラム
         if '/ochinpo' in msg: # おちんぽが入っているとき( ◜◡＾)っ✂╰⋃╯
-            msg = msg.split()[1:]
+            msg = ''.join(msg.split()[1:])
             PATTERN = r'<:[0-9|a-z|_]+:[0-9]+>'
             query = re.sub(PATTERN, "0", msg)
             emoji = re.findall(PATTERN, msg)
