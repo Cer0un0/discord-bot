c34cfea9328bb60db021ab9756b7fd417c7e5515
satory074 <satory074@gmail.com>
2019-10-23 00:37:15 +0900

    debug

--- bot.py
+++ bot.py
@@ -197,7 +197,7 @@ async def on_message(message):
 
     # 1行ずつ処理
     for msg in message.content.split('\n'):
-        await message.channel.send(re.match(':(.+):(\d+)', msg).group())
+        await message.channel.send(re.match('<:(.+):(\d+)>', msg).group())
         break
 
         # 1回だけの応答用
