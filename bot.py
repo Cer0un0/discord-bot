07315c0857815b864010bcd5f11d52a193d4e9ef
satory074 <satory074@gmail.com>
2019-10-25 00:18:06 +0900

    debug

--- bot.py
+++ bot.py
@@ -254,8 +254,8 @@ async def on_message(message):
 
                 reply = ""
                 for i, r in enumerate(li_reply):
-                    # reply += li_query[li_reply.index(r)]
-                    await message.channel.send(li_reply.index(r))
+                    reply += li_query[li_query.index(r)]
+                    # await message.channel.send(li_reply.index(r))
 
                     if (i+1) % 50 == 0:
                         await message.channel.send(reply)
