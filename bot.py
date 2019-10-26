d526ca8da6e4e03f06df330d4947d7ffbca3015a
satory074 <satory074@gmail.com>
2019-10-26 13:40:24 +0900

    debug

--- bot.py
+++ bot.py
@@ -230,34 +230,41 @@ async def on_message(message):
             # ターゲットの文字列リスト（カスタム絵文字＋文字）
             li_target = [emoji.pop(0) if q == '-' else q for q in list(target)]
 
-            if len(li_target) > 4:  # おちんぽおっきいときは処理してあげない
+            # ちっちゃいおちんぽだけ処理
+            if len(li_target) > 4:
                 await message.channel.send("おちんぽおっきすぎだよぉ...")
-            else:  # おちんぽちっちゃいときは処理
+            else:
+                li_reply = [] # ランダム文字リスト
+                li_dumy_target = [f"unbo{i}" for i in range(len(li_target))] # あとで置換される文字リスト
+
+                len_ = len(li_dumy_target)
+                str_target = "".join(li_dumy_target)
+
                 cnt = 0
                 is_proc = True
-                li_dumy_target = [f"unbo{i}" for i in range(len(li_target))]
-                len_ = len(li_dumy_target)
-                li_reply = []
-                target = "".join(li_dumy_target)
-
-                while ''.join(li_reply[-len_:]) != target:
+                while is_proc:
                     # おちんぽシコリすぎないようにする
                     if cnt > 114514:
                         break
 
                     li_reply.append(ra.choice(len_))
+                    # ケツの文字がターゲットwordと一致していたらループbooleanをFalseに
+                    is_proc = ''.join(li_reply[-len_:]) != str_target
 
                     cnt += 1
+                else:
+                    # 実際の文字に置換
+                    reply = ""
+                    for i, r in enumerate(li_reply):
+                        reply += li_target[li_dumy_target.index(r)]
 
-                reply = ""
-                for i, r in enumerate(li_reply):
-                    reply += li_target[li_dumy_target.index(r)]
-
-                    if (i+1) % 50 == 0:
+                        # 50文字ごとに出力
+                        if (i+1) % 50 == 0:
+                            await message.channel.send(reply)
+                            reply = ""
+                    else:
                         await message.channel.send(reply)
-                        reply = ""
-                await message.channel.send(reply)
-                await message.channel.send(f"おぉぉおﾞおﾞ～っ！！イグゥウ！！イッグゥウウ！！{cnt}回目で果てました...")
+                        await message.channel.send(f"おぉぉおﾞおﾞ～っ！！イグゥウ！！イッグゥウウ！！{cnt}回目で果てました...")
 
         # if message.content.startswith('/ommc'):
         #    channel = client.get_channel('nyr')
