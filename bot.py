d2594ec74263c9456e0091cf457621b99c00b1d3
satory074 <satory074@gmail.com>
2019-10-27 17:32:46 +0900

    debug

--- bot.py
+++ bot.py
@@ -217,8 +217,8 @@ def writeCsv(data, fname='VirtualContest.csv'):
         writer.writerows(data)
 
 
-# csv = readCsv()
-# current_vc = csv[-1]
+csv = readCsv()
+current_vc = csv[-1]
 
 # メッセージ受信時に動作する処理
 @client.event
