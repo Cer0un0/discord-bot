afdcb676d28330d415d83417bb248c693cd6d9ae
satory074 <satory074@gmail.com>
2019-10-27 17:03:04 +0900

    debug

--- bot.py
+++ bot.py
@@ -290,7 +290,7 @@ async def on_message(message):
                 await message.channel.send(f"おぉぉおﾞおﾞ～っ！！イグゥウ！！イッグゥウウ！！{cnt}回目で果てました...")
 
         if len(msg.split()) == 3:
-            await message.channel.send(msg.split()[-1])
+            await message.channel.send(mmsg.split()[-1] == '#atcoderVirtualContest')
             if msg.split()[-1] == '#atcoderVirtualContest':
                 await message.channel.send(msg.split()[1])
                 get_url_info = requests.get(msg.split()[1])
