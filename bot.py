86e0064b2acba4ae835c877ed90ad72550875a5e
satory074 <satory074@gmail.com>
2019-10-30 20:51:11 +0900

    debug

--- bot.py
+++ bot.py
@@ -355,7 +355,7 @@ async def on_ready():
         channel = client.get_channel(632106376427995137) # 開発室
         #     channel = client.get_channel(615869415103266817) # AtCoder
         #     await channel.send("【新うんぼぼ杯】始まりましたhttps://not-522.appspot.com/contest/5937364687912960")
-        dt_now_jst = timezone('Asia/Tokyo').localize(dt.now())
+        dt_now_jst = dt.now()
         # await channel.send(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
         # await channel.send(time.strftime("%Y/%m/%d %H:%M:%S", dt_now_jst))
         await channel.send(dt_now_jst)
