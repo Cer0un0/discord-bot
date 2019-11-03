30477d36f1afb8a92dcb1a6b7a153e79c44aa09a
satory074 <satory074@gmail.com>
2019-11-03 22:09:44 +0900

    debug

--- bot.py
+++ bot.py
@@ -318,9 +318,9 @@ async def on_message(message):
             with open("vc_alert.txt") as f:
                 lines = [s.strip() for s in f.readlines()]
 
-            lines.insert(f"{title}, {t_start}, {t_end}, {link}")
-
             await message.channel.send(lines)
+
+            lines.insert(f"{title}, {t_start}, {t_end}, {link}")
 
             with open("vc_alert.txt", mode='w') as f:
                 f.writelines(lines)
